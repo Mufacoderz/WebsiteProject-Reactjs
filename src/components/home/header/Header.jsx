774a3b0fd6543c5d760326af74e91d1c8aeb7750
Muhammad <mf9555759@gmail.com>
2025-08-10 20:27:07 +0800

    add assets

---
+++ src/components/home/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+const Header = () =>{
+    return(
+        <>
+            <header className='header'>
+                <div className="container flex">
+                    <div className="logo">
+                        <img src="assest/logo.png" alt="" />
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
