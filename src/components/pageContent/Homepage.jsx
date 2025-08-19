4af07cf43e4068f0ea31d3811273288f4eb2482a
Muhammad <mf9555759@gmail.com>
2025-08-19 16:16:55 +0800

    about

--- src/components/pageContent/Homepage.jsx
+++ src/components/pageContent/Homepage.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Home from '../home/homes/Home.jsx' 
 import Branding from '../home/Branding.jsx'
-  
+import About from '../home/about/About.jsx'
 
 const Homepage = () => {
   return (
     <>
         <Home />
         <Branding />
+        <About />
     </>
   
   )
