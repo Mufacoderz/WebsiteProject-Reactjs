0f8aeec8b2c5722ea721434e09631689ee99e3dc
Muhammad <mf9555759@gmail.com>
2025-08-19 16:52:49 +0800

    services

--- src/components/home/services/Services.jsx
+++ src/components/home/services/Services.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
+import ServicesData from './ServicesData'
 
 const Services = () => {
+
+  
+
   return (
     <>
       <section className='services topMargin'>
         <div className="container">
           <div className="heading">
-            
+            <h3>MY SERVICES</h3>
+            <h1>Interactive Services Offered Me</h1>
+          </div>
+          <div className="containe grid topMargin">
+            {ServicesData.map((val)=>{
+              return(
+                <div className='box'>
+                  <div className="img">
+                    <img src={val.cover} alt="" />
+                  </div>
+                  <div className="text">
+                    <h2>{val.title}</h2>
+                    <h2>{val.desc}</h2>
+                  </div>
+                </div>
+              )
+            })}
           </div>
         </div>
       </section>
---
+++ src/components/home/services/ServicesData.js
@@ -0,0 +1,49 @@
+// src/components/home/services/ServicesData.js
+
+import img1 from "../../../assets/services/services-1.jpg"
+import img2 from "../../../assets/services/services-2.jpg"
+import img3 from "../../../assets/services/services-3.jpg"
+import img4 from "../../../assets/services/services-4.jpg"
+import img5 from "../../../assets/services/services-5.jpg"
+import img6 from "../../../assets/services/services-6.jpg"
+
+const ServicesData = [
+  {
+    id: 1,
+    title: "Web Design",
+    desc: "Lorem ipsum dolor sit amet, consecte tuer adipiscing elit enean.",
+    cover: img1,
+  },
+  {
+    id: 2,
+    title: "Web Development",
+    desc: "Lorem ipsum dolor sit amet, consecte tuer adipiscing elit enean.",
+    cover: img2,
+  },
+  {
+    id: 3,
+    title: "Photography",
+    desc: "Lorem ipsum dolor sit amet, consecte tuer adipiscing elit enean.",
+    cover: img3,
+  },
+  {
+    id: 4,
+    title: "Live Support",
+    desc: "Lorem ipsum dolor sit amet, consecte tuer adipiscing elit enean.",
+    cover: img4,
+  },
+  {
+    id: 5,
+    title: "Content Writing",
+    desc: "Lorem ipsum dolor sit amet, consecte tuer adipiscing elit enean.",
+    cover: img5,
+  },
+  {
+    id: 6,
+    title: "Timely Deliverables",
+    desc: "Lorem ipsum dolor sit amet, consecte tuer adipiscing elit enean.",
+    cover: img6,
+  },
+]
+
+export default ServicesData
